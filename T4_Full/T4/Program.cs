21697a84cb6a091c798c188228f4a4600a6884b5
qwe <qwe>
2022-10-18 16:12:39 +0300

    ***

---
+++ T4_Full/T4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+///////////////////////////////////////////
+////////////////////////////////////////////
+namespace T4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("nfhjasdgjhd");
+            T4.RuntimeTextTemplate1.Test();
+            //T4.RuntimeTextTemplate1.Test();
+            /*
+            System.Threading.Tasks.Task _Task_1 = null;
+            _Task_1 = System.Threading.Tasks.Task.Run(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    System.Console.WriteLine("Task_1" + i);
+                }
+            });
+            System.Threading.Tasks.Task _Task_2 = null;
+            _Task_2 = System.Threading.Tasks.Task.Run(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    System.Console.WriteLine("Task_2" + i);
+                }
+            });
+            _Task_1.Wait();
+            _Task_2.Wait();
+            */
+            System.Console.ReadLine();
+            
+        }
+    }
+}
---
+++ T4_Full/T4/TextTemplate6.cs
@@ -0,0 +1,18 @@
+﻿//qweqw =)
+namespace System
+{
+    public class EXT_delegate
+    {
+        public delegate MyDel MyDel(System.String _STR, int __Value);
+    }
+}
+//Привет мир 0
+//Привет мир 1
+//Привет мир 2
+//Привет мир 3
+//Привет мир 4
+//Привет мир 5
+//Привет мир 6
+//Привет мир 7
+//Привет мир 8
+//Microsoft.VisualStudio.TextTemplating53E8AB195DE477B62E0DCEFE2AA8A8D4EB3DEEB9DE3C356DAD6E41EC029E648923DF8AA3BBAF90B06F555FC5A497DEF58B3FA530917DE4D7C232583AB1126B9B.GeneratedTextTransformation
